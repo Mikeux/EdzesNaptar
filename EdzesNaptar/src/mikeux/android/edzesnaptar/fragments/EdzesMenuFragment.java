1a3bc1f405f71226b77d07789712de0f6260e755
Mikeux <mikeux@gmail.com>
2014-09-12 16:34:55 +0200

    Menü ikonok

--- EdzesNaptar/src/mikeux/android/edzesnaptar/fragments/EdzesMenuFragment.java
+++ EdzesNaptar/src/mikeux/android/edzesnaptar/fragments/EdzesMenuFragment.java
@@ -1,7 +1,11 @@
 package mikeux.android.edzesnaptar.fragments;
 
+import java.util.ArrayList;
+
 import mikeux.android.edzesnaptar.R;
 import mikeux.android.edzesnaptar.ResponsiveUIActivity;
+import mikeux.android.edzesnaptar.util.EdzesMenuList;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
@@ -12,22 +16,53 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+//http://www.tutorialsbuzz.com/2014/05/android-listfragment-using-baseadapter.html
+
 public class EdzesMenuFragment extends ListFragment {
-	
-	//http://www.tutorialsbuzz.com/2014/05/android-listfragment-using-baseadapter.html
-	
+    String[] menutitles;
+    TypedArray menuIcons;
+    public EdzesMenuList adapter;
+    
+	private ArrayList<String> nevek = new ArrayList<String>();
+	private ArrayList<Integer> kepek = new ArrayList<Integer>();
+    
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.list, null);
+		return inflater.inflate(R.layout.fragment_menu, null);
 	}
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		String[] birds = getResources().getStringArray(R.array.menu_fragment);
+		
+		/*TypedArray typedarr = getResources().obtainTypedArray(R.array.menu_fragment);
+		
+		for(int i=0;i<typedarr.length();i++) {
+			nevek.add(typedarr.getResourceId(i, -1).);
+		}
+		
+        menutitles = getResources().getStringArray(R.array.menu_fragment);
+        menuIcons = getResources().obtainTypedArray(R.array.menu_icons);*/
+
+        nevek.add("Edzések");
+        nevek.add("Edzés fajták");
+        nevek.add("Edzés tervek");
+        nevek.add("Statisztikák");
+        nevek.add("Kilépés");
+        
+        kepek.add(R.drawable.alma);
+        kepek.add(R.drawable.kulacs);
+        kepek.add(R.drawable.stopper);
+        kepek.add(R.drawable.sziv);
+        kepek.add(-1);
+        
+        adapter = new EdzesMenuList(this.getActivity(), nevek, kepek);
+        setListAdapter(adapter);
+        
+		/*String[] birds = getResources().getStringArray(R.array.menu_fragment);
 		ArrayAdapter<String> menuAdapter = new ArrayAdapter<String>(getActivity(), 
 				android.R.layout.simple_list_item_1, android.R.id.text1, birds);
-		setListAdapter(menuAdapter);
+		setListAdapter(menuAdapter);*/
 	}
 	
 	@Override
---
+++ EdzesNaptar/src/mikeux/android/edzesnaptar/util/EdzesMenuList.java
@@ -0,0 +1,38 @@
+package mikeux.android.edzesnaptar.util;
+
+import java.util.ArrayList;
+
+import mikeux.android.edzesnaptar.R;
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class EdzesMenuList  extends ArrayAdapter<String> {
+	private final Activity context;
+	private final ArrayList<String> nev;
+	private final ArrayList<Integer> imageId;
+	
+	public EdzesMenuList(Activity context,	ArrayList<String> nev, ArrayList<Integer> imageId) {
+		super(context, R.layout.list_item_menu, nev);
+		this.context = context;
+		this.nev = nev;
+		this.imageId = imageId;
+	}
+	@Override
+	public View getView(final int position, View view, ViewGroup parent) {    
+		LayoutInflater inflater = context.getLayoutInflater();
+		View rowView= inflater.inflate(R.layout.list_item_menu, null, true);
+		
+		TextView txtTitle = (TextView) rowView.findViewById(R.id.title);
+		txtTitle.setText(nev.get(position));
+		
+		ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+		imageView.setImageResource(imageId.get(position));
+
+		return rowView;
+	}
+}
