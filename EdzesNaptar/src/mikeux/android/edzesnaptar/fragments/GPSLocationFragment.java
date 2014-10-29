51b9e49af4c4ee492a3f4651f1479b42af49063c
Mikeux <mikeux@gmail.com>
2014-10-29 15:24:54 +0100

    GPS linkek

---
+++ EdzesNaptar/src/mikeux/android/edzesnaptar/fragments/GPSLocationFragment.java
@@ -0,0 +1,15 @@
+package mikeux.android.edzesnaptar.fragments;
+
+/*
+Google: android GPS tracking programming
+http://www.codeproject.com/Articles/665527/A-GPS-Location-Plotting-Android-Application
+http://www.javabeat.net/location-tracker-in-android-using-gps-positioning-and-sqlite/
+http://www.androidhive.info/2012/07/android-gps-location-manager-tutorial/
+http://www.androidsnippets.com/distance-between-two-gps-coordinates-in-meter
+http://developer.android.com/reference/android/location/Location.html#distanceTo(android.location.Location)
+http://blog.doityourselfandroid.com/2010/12/25/understanding-locationlistener-android/
+ */
+
+public class GPSLocationFragment {
+
+}
